34e39c8ec014d341c14dbe219860e24a7830e31b
Maday <madaydomtt@gmail.com>
2021-09-05 00:42:45 -0600

    Change templates and static

--- DevBarber/settings.py
+++ DevBarber/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'DevBarber.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 """
 import dj_database_url
 from decouple import config
@@ -90,7 +90,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
